d1068c440aac4d4ab29e6176162383bb191d84dc
shiggins113 <shiggins@pivotal.io>
2016-06-08 09:16:12 +0100

    Add files via upload

---
+++ gss-ticket-manager-poller-service/src/main/java/com/gss/ticket/manager/GssTicketManagerPollerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gss.ticket.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GssTicketManagerPollerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GssTicketManagerPollerServiceApplication.class, args);
+	}
+}
