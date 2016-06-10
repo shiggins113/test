43e94f936fcfd53722c57de7c99a9b52a9c9fb27
shiggins113 <shiggins@pivotal.io>
2016-06-10 11:42:10 +0100

    Add files via upload

---
+++ src/main/java/io/pivotal/ticket/TicketManagerPollerApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagerPollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagerPollerApplication.class, args);
+	}
+}
